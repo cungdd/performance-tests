0a1dd59c25723aacf3b12c3020195250d769cb2b
Cungdd <Cungdd.bkhn@gmail.com>
2025-06-21 09:55:15 +0700

    add device smart light

---
+++ src/main/java/org/thingsboard/tools/service/msg/smartlight/SmartLightAttributesGenerator.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright © 2016-2025 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.tools.service.msg.smartlight;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import org.thingsboard.tools.service.msg.MessageGenerator;
+import org.thingsboard.tools.service.msg.Msg;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(prefix = "test", value = "payloadType", havingValue = "SMART_LIGHT")
+public class SmartLightAttributesGenerator extends BaseSmartLightGenerator implements MessageGenerator {
+
+    @Override
+    public Msg getNextMessage(String deviceName, boolean shouldTriggerAlarm) {
+        byte[] payload;
+        try {
+            ObjectNode data = mapper.createObjectNode();
+            ObjectNode values;
+            if (isGateway()) {
+                values = data.putObject(deviceName);
+            } else {
+                values = data;
+            }
+            values.put("model", "SL-100");
+            values.put("firmwareVersion", "1.2.3");
+            payload = mapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            log.warn("Failed to generate message", e);
+            throw new RuntimeException(e);
+        }
+        return new Msg(payload);
+    }
+} 
---
+++ src/main/java/org/thingsboard/tools/service/msg/smartlight/SmartLightTelemetryGenerator.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright © 2016-2025 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.tools.service.msg.smartlight;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import org.thingsboard.tools.service.msg.MessageGenerator;
+import org.thingsboard.tools.service.msg.Msg;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(prefix = "test", value = "payloadType", havingValue = "SMART_LIGHT")
+public class SmartLightTelemetryGenerator extends BaseSmartLightGenerator implements MessageGenerator {
+
+    @Override
+    public Msg getNextMessage(String deviceName, boolean shouldTriggerAlarm) {
+        byte[] payload;
+        try {
+            ObjectNode data = mapper.createObjectNode();
+            ObjectNode tsNode;
+            if (isGateway()) {
+                ArrayNode array = data.putArray(deviceName);
+                tsNode = array.addObject();
+            } else {
+                tsNode = data;
+            }
+            tsNode.put("ts", System.currentTimeMillis());
+            ObjectNode values = tsNode.putObject("values");
+            values.put("onoff", random.nextInt(2));
+            values.put("dim", random.nextInt(101));
+            values.put("cct", random.nextInt(256));
+            payload = mapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            log.warn("Failed to generate message", e);
+            throw new RuntimeException(e);
+        }
+        return new Msg(payload, false);
+    }
+} 
